23d9ab3a92fd53d6feba21e33b319401aef984c4
Orawko <bartlomiej.orawiec@gmail.com>
2020-06-14 21:10:28 +0200

    add tests, code refactor

--- react_client/src/setupTests.js
+++ react_client/src/setupTests.js
@@ -3,3 +3,25 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import {validateName, validatePassword} from "./helpers/validate";
+
+describe("validateName", () => {
+  it("returns true if the string is longer than 3", () => {
+    expect(validateName("abba")).toEqual(true);
+  });
+  it("returns false if the string is shorter than 3", () => {
+    expect(validateName("ab")).toEqual(false);
+  });
+});
+
+describe("validatePassword", () => {
+  it("returns true if passwords are equal and longer than 3", () => {
+    expect(validatePassword("abba", "abba")).toEqual(true);
+  });
+  it("returns false if passwords are different", () => {
+    expect(validatePassword("ab", "abb")).toEqual(false);
+  });
+  it("returns false if passwords are too short", () => {
+    expect(validatePassword("abb", "abb")).toEqual(false);
+  });
+});
